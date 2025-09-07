ce35c4d10f0910c4a8b5129494aabf5aa23dcab2
alexcreamer <creamerprojects@gmail.com>
2025-09-07 09:51:19 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
             <Route path="/" element={<PublicRoute><Index /></PublicRoute>} />
             <Route path="/auth" element={<PublicRoute><Auth /></PublicRoute>} />
